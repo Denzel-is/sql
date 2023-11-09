c280fdbd0cccbf6d0d905a8ab6b38119c0d731f2
unknown <danilaborodins@gmail.com>
2023-11-09 13:07:48 +0600

    hghghgh

--- 1.py
+++ 1.py
@@ -1,1 +1,25 @@
-assa
+import psycopg2
+
+conn = psycopg2.connect(
+        host='localhost',
+        database='W11',
+        user='postgres',
+        password='123',
+        port='5432'
+    )
+cur = conn.cursor()
+cur.execute("SELECT first_name FROM mock_data LIMIT 5;")
+unames=[r[0] for r in cur.fetchall()]
+uname = input("Enter ur login ")
+founds = False
+while not founds:
+    if uname in unames:
+        print("u have in the list")
+        founds = True
+    else:
+        print("u have not in the list")
+        founds = True
+
+conn.commit()
+cur.close()    
+conn.close()
