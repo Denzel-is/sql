21315da466ea7a2278a6e99a746e52623fb9b2cc
admin <admin@aitu.kz>
2023-11-10 09:31:50 +0600

    v1.6 binarySearch

--- 1.py
+++ 1.py
@@ -1,25 +1,43 @@
 import psycopg2
 
-conn = psycopg2.connect(
-        host='localhost',
-        database='W11',
-        user='postgres',
-        password='123',
-        port='5432'
-    )
-cur = conn.cursor()
-cur.execute("SELECT first_name FROM mock_data LIMIT 5;")
-unames=[r[0] for r in cur.fetchall()]
-uname = input("Enter ur login ")
-founds = False
-while not founds:
-    if uname in unames:
-        print("u have in the list")
-        founds = True
-    else:
-        print("u have not in the list")
-        founds = True
 
-conn.commit()
-cur.close()    
+
+
+def binarySearch(unames, x, low, high):
+
+    while low <= high:
+        mid = low + (high - low)//2
+
+        if unames[mid] == x:
+            return mid
+
+        elif unames[mid] < x:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='SUBD',
+    user='postgres',
+    password='123',
+    port='5432'
+)
+
+cur = conn.cursor()
+cur.execute("SELECT name FROM fruits LIMIT 5;")
+unames = [r[0] for r in cur.fetchall()]
+
+uname = input("Enter your login (name of the fruit): ")
+found = binarySearch(unames, uname)
+
+if found:
+    print("You have it in the list.")
+else:
+    print("You do not have it in the list.")
+
+cur.close()
 conn.close()
